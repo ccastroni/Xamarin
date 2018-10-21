988695171884eded544bdd92702e79802cb1602f
ccastroni <claudio.castroni@hotmail.cl>
2018-10-21 18:20:59 -0300

    Login Page
    Primera pagina de login

---
+++ lands/Lands/Lands/Infrastructure/IntanceLocater.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+    
+    public class IntanceLocater
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public  IntanceLocater()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ lands/Lands/Lands/Infrastructure/IntancesLocater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Lands.Infrastructure
+{
+	public class IntancesLocater : ContentPage
+	{
+		public IntancesLocater ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to Xamarin.Forms!" }
+				}
+			};
+		}
+	}
+}
